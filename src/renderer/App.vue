402abe7f9b8a9a36820b359f7b01773197157c12
zhuruhong <zhuruhong@didichuxing.com>
2019-10-07 22:21:09 +0800

    通过vue脚手架构造项目 vue init simulatedgreg/electron-vue pipeline

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'pipeline'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
